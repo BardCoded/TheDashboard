20776972955b0c154cbf2b455bd91ad7b605ef5f
Markus Johansson <markus@enkelmedia.se>
2025-11-13 08:07:42 +0100

    updates for v17
    (cherry picked from commit 679f46dfab3eaa02555139656d219098f434b82d)

--- src/Our.Umbraco.TheDashboard/Client/openapi-ts.config.ts
+++ src/Our.Umbraco.TheDashboard/Client/openapi-ts.config.ts
@@ -8,34 +8,13 @@ export default defineConfig({
 	plugins: [
     ...defaultPlugins,
 		{
-			name: '@hey-api/client-fetch',
-			bundle: false,
-			exportFromIndex: true,
-			throwOnError: true,
-		},
-		{
 			name: '@hey-api/typescript',
 			enums: 'typescript'
 		},
 		{
 			name: '@hey-api/sdk',
 			asClass: true,
-      serviceNameBuilder : '{{name}}Resource'
+      classNameBuilder : '{{name}}Resource'
 		}
 	]
 });
-
-/*
-export default defineConfig({
-  input: 'http://localhost:24760/umbraco/swagger/the-dashboard/swagger.json',
-  output: 'src/backend-api',
-  //format : false,
-  //enums : 'javascript', // Typescript not recommended https://heyapi.vercel.app/openapi-ts/configuration.html#enums
-  schemas : false,
-  //lint : false,
-  //debug: true,
-  services: {
-    name: '{{name}}Resource'
-  }
-});
-*/
