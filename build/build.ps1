3c6b508883f2eafaee431bbb02d40c449c825f46
Markus Johansson <markus@enkelmedia.se>
2026-05-20 08:11:00 +0200

    Ensure NPM packages are up to date

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,4 @@
-param ($version = "17.0.0",$versionSuffix = "")
+param ($version = "18.0.0",$versionSuffix = "-beta1")
 # Params
 # version = major.minor.patch
 # versionSuffix = eg -beta1, -rc1 (should include leading -). Leave empty string if not a pre-release.
